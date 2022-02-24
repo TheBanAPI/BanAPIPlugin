e820588ec68efd475aae507c3469d585f7e656a9
TheCurano <thecurano@outlook.de>
2022-02-24 18:22:15 +0100

    Fixed Message Mistakes

--- src/main/java/de/curano/banapiplugin/spigot/commands/BanAPICommand.java
+++ src/main/java/de/curano/banapiplugin/spigot/commands/BanAPICommand.java
@@ -12,9 +12,9 @@ public class BanAPICommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (Config.config.getBoolean("enabled", true)) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8» &4BanAPI &8┃ &7Das &eBanAPI-Plugin&7 ist nun &aaktiviert&7."));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8» &4BanAPI &8┃ &7Das &eBanAPI-Plugin&7 ist &aaktiviert&7."));
         } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8» &4BanAPI &8┃ &7Das &eBanAPI-Plugin&7 ist nun &cdeaktiviert&7."));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8» &4BanAPI &8┃ &7Das &eBanAPI-Plugin&7 ist &cdeaktiviert&7."));
         }
         return true;
     }
